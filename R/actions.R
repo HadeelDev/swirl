977ef35313f9e69f733d3d2dfd8c242b54f05ef4
Hadeel <hadeelaljohani4@gmail.com>
2018-07-21 09:23:19 +1000

    translate4

--- R/actions.R
+++ R/actions.R
@@ -11,7 +11,7 @@ do_nxt.default <- function(e) {
   #  their "official" values, in case the user has changed them
   #  while playing.
   if(length(e$snapshot)>0)xfer(as.environment(e$snapshot), globalenv())
-  swirl_out(s()%N%"Resuming lesson...")
+  swirl_out(s()%N%"لنبدا الدرس")
   e$playing <- FALSE
   e$iptr <- 1  
 }
@@ -20,7 +20,7 @@ do_reset.default <- function(e) {
   e$playing <- FALSE
   e$reset <- TRUE
   e$iptr <- 2
-  swirl_out(s()%N%"I just reset the script to its original state. If it doesn't refresh immediately, you may need to click on it.", 
+  swirl_out(s()%N%"لقد قمت بأعاده تعيين البرنامج النصي الي حالته الاصليه ،اذا لم يتم تحديثه على الفور ،قد يحتاج الى النقر عليه", 
             skip_after = TRUE)
 }
 
@@ -30,17 +30,17 @@ do_submit.default <- function(e) {
   e$script_contents <- readLines(e$script_temp_path, warn = FALSE)
   # Save expr to e
   e$expr <- try(parse(text = e$script_contents), silent = TRUE)
-  swirl_out(s()%N%"Sourcing your script...", skip_after = TRUE)
+  swirl_out(s()%N%"مصادر البرنامج النصي الخاص بك", skip_after = TRUE)
   try(source(e$script_temp_path, encoding = "UTF-8"))
 }
 
 do_play.default <- function(e) {
-  swirl_out(s()%N%"Entering play mode. Experiment as you please, then type nxt() when you are ready to resume the lesson.", skip_after=TRUE)
+  swirl_out(s()%N%"الان سوف ندخل ساحه اللعب ، اضغط على زر المتابعه اذا كنت مستعدا لاستذناف الدرس", skip_after=TRUE)
   e$playing <- TRUE
 }
 
 do_main.default <- function(e) {
-  swirl_out(s()%N%"Returning to the main menu...")
+  swirl_out(s()%N%"رجوع للقائمه الرئيسيه")
   # Remove the current lesson. Progress has been saved already.
   if(exists("les", e, inherits=FALSE)){
     rm("les", envir=e, inherits=FALSE)
@@ -48,5 +48,5 @@ do_main.default <- function(e) {
 }
 
 do_restart.default <- function(e) {
-  swirl_out(s()%N%"This feature is not implemented yet for Swirl.")
+  swirl_out(s()%N%"هذه الميزه لم تفعل في برنامح سويل الى الان")
 }
