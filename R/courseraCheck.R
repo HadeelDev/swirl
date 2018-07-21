517a61e3d4884a13b9ed8deb1f1bed5af73eb595
Hadeel <hadeelaljohani4@gmail.com>
2018-07-21 16:24:44 +1000

    translation4

--- R/courseraCheck.R
+++ R/courseraCheck.R
@@ -10,24 +10,24 @@ courseraCheck <- function(e){
   baseurl <- paste0("http://", partner, ".coursera.org/")
 
   tt <- c(rep(letters, 3), seq(100))
-  swirl_out(s()%N%"Are you currently enrolled in the Coursera course associated with this lesson?")
-  yn <- select.list(c("Yes","No"), graphics=FALSE)
-  if(yn=="No")return()
+  swirl_out(s()%N%"المرتبطه بهذا الدرس؟ Coursera هل انت حاليا مسجل في دوره")
+  yn <- select.list(c("نعم","لا"), graphics=FALSE)
+  if(yn=="لا")return()
   ss <- lapply(1:2, function(i) {
     paste0(sample(tt, sample(seq(400), 1), replace=TRUE), collapse="")
   })
-  swirl_out(s()%N%"Would you like me to notify Coursera that you've completed this lesson?",
-            "If so, I'll need to get some more info from you.")
-  choice <- select.list(c("Yes","No","Maybe later"), graphics=FALSE)
-  if(choice=="No") return()
+  swirl_out(s()%N%"انك قد اكملت الدرس؟ Coursera هل تريد مني ان ابلغ كورس",
+            "اذا نعم، سوف اخذ منك مزيدا من المعلومات ")
+  choice <- select.list(c("احتمال لاحقا","لا","نعم"), graphics=FALSE)
+  if(choice=="لا") return()
   # Begin submission loop
   ok <- FALSE
   while(!ok) {
     # Get submission credentials
     r <- getCreds(e)
-    email <- r["email"]
-    passwd <- r["passwd"]
-    course_name <- r["courseid"]
+    email <- r["ايميل"]
+    passwd <- r["كلمه المرور"]
+    course_name <- r["اسم الكورس"]
     output <- paste0(ss[[1]], substr(e$coursera, 1, 16), ss[[2]],
                      collapse="")
     # Name output file
@@ -35,10 +35,10 @@ courseraCheck <- function(e){
     # Write output to text file
     writeLines(output, output_filename)
     # If going straight to manual submission, then exit loop.
-    if(choice=="Maybe later") ok <- TRUE
+    if(choice=="احتمال لاحقا") ok <- TRUE
     # If doing automatic submission, then give it a try.
-    if(choice=="Yes"){
-      swirl_out(s()%N%"I'll try to tell Coursera you've completed this lesson now.")
+    if(choice=="نعم"){
+      swirl_out(s()%N%"انك قد اكملت الدرس الان Coursera سوف احاول الان ان ابلغ كورس")
       challenge.url <- paste(baseurl, course_name,
                              "assignment/challenge", sep = "/")
       submit.url <- paste(baseurl, course_name,
@@ -56,12 +56,12 @@ courseraCheck <- function(e){
                                   signature=ch$state)
         # If incorrect, empty string will be returned
         if(!length(results)) {
-          swirl_out(s()%N%"You skipped too many questions! You'll need to complete",
-                    s()%N%"this lesson again if you'd like to receive credit. Please",
-                    s()%N%"don't skip more than one question next time.")
+          swirl_out(s()%N%"لقد تجاهلت الكثير من الاسئله التي تحتاج الي اكمالها",
+                    s()%N%"اذا كنت ترغب بالحصول علي مزيد من النقاط فهذا هو الدرس مره اخرى",
+                    s()%N%"لا تتجاهل اكثر من سؤال في المره القادمه")
           return()
         }
-        if(!is(results, "try-error")){
+        if(!is(results, "حاول ،خطأ")){
           # TODO: It would be best to detect success here, rather than
           # failure, but as of Feb 23 2014, submit.url may not throw
           # an error indicating failure but instead return an HTML
@@ -71,35 +71,35 @@ courseraCheck <- function(e){
           # be course dependent, so some standard handshake will have
           # to be set up eventually.
           swirl_out(results)
-          if(!str_detect(results, "[Ee]xception")){
-            swirl_out(paste0(s()%N%"I've notified Coursera that you have completed ",
+          if(!str_detect(results, "[Ee]توقع")){
+            swirl_out(paste0(s()%N%"بانك قد اكملت الدرس Coursera لقد اخبرت ",
                              course_name, ", ", lesson_name,"."))
             # Remove manual submission text file
             unlink(output_filename)
             # Exit loop since submission successful
             return()
           }
-          swirl_out(s()%N%"I'm sorry, something went wrong with automatic submission.")
+          swirl_out(s()%N%"انا اسف ، يوجد خطأ في ادخال البيانات")
           # Exit loop if user doesn't want to retry auto submission
           ok <- !retry()
         } else {
-          swirl_out(s()%N%"I'm sorry, something went wrong with automatic submission.")
+          swirl_out(s()%N%"انا اسف ، يوجد خطأ في ادخال البيانات")
           # Exit loop if user doesn't want to retry auto submission
           ok <- !retry()
         }
       } else {
-        swirl_out(s()%N%"I'm sorry, something went wrong with establishing connection.")
+        swirl_out(s()%N%"انا اسف ، يوجد خطأ في  انشاء الاتصالات")
         # Exit loop if user doesn't want to retry auto submission
         ok <- !retry()
       }
     } # end of yes branch
   } # end of while loop
-  swirl_out(s()%N%"To notify Coursera that you have completed this lesson,",
-            s()%N%"please upload", sQuote(output_filename),
-            s()%N%"to Coursera manually. You may do so by visiting the Programming",
-            s()%N%"Assignments page on your course website and selecting the Submit",
-            s()%N%"button next to the appropriate swirl lesson.",
-            s()%N%"I've placed the file in the following directory:",
+  swirl_out(s()%N%"انك قد اكملت الدرس Coursera لاعلام",
+            s()%N%"يرجى التحميل", sQuote(output_filename),
+            s()%N%"يدويا قد يتوجب عليك زياره البرمجه Coursera لتفعيل  ",
+            s()%N%"صفحه الواجبات على موقع الدوره الخاصه بك وايضا رابط تسليم الواجبات",
+            s()%N%" swirlرز بجانب درس ",
+            s()%N%":قمت بوضع الملف في المكان التالي",
             skip_after=TRUE)
   message(getwd(), "\n")
   readline("...")
@@ -107,20 +107,20 @@ courseraCheck <- function(e){
 
 # Returns TRUE if user would like to retry, FALSE if not
 retry <- function() {
-  swirl_out(s()%N%"Would you like to retry automatic submission or just submit manually?")
-  ans <- select.list(c("Retry automatic submission", "Submit manually"), graphics=FALSE)
+  swirl_out(s()%N%"هل تريد اعاده المحاوله التلقائيه او ارسالها يدويا؟")
+  ans <- select.list(c("تسليم يدوي", "اعاده المحاوله التلقائيه"), graphics=FALSE)
   # Return TRUE if user would like to retry
-  return(ans == "Retry automatic submission")
+  return(ans == "اعاده المحاوله التلقائيه")
 }
 
 get_courseid <- function() {
-  swirl_out(s()%N%"The first item I need is your Course ID. For example, if the",
-            s()%N%"homepage for your Coursera course was",
+  swirl_out(s()%N%"البند الاول الذي احتاجه هو معرفه الدوره التدريبه الخاصه بك، على سبيل المثال :لو",
+            s()%N%"الخاصه بك كانت Coursera صفحه كورس",
             s()%N%"'https://class.coursera.org/rprog-001',",
-            s()%N%"then your course ID would be 'rprog-001' (without the quotes).",
+            s()%N%"اذن سوف يكون رقم الدوره الخاصه بك هو 'rprog-001' (without the quotes).",
             skip_after=TRUE)
   repeat {
-    courseid <- readline("Course ID: ")
+    courseid <- readline(":رقم الدوره")
     # Remove quotes if there are any
     courseid <- gsub("\'|\"", "", courseid)
     # Set up test cases
@@ -135,34 +135,33 @@ get_courseid <- function() {
     } else {
       # Check if courseid is a url
       if(is_url) {
-        swirl_out(s()%N%"It looks like you entered a web address, which is not what I'm",
-                  s()%N%"looking for.")
+        swirl_out(s()%N%"يبدو انك قمت بإدخال عنوان ويب وليس ما انا عليه",
+                  s()%N%"تبحث عن")
       }
       # Check if courseid is all numbers
       if(is_numbers) {
-        swirl_out(s()%N%"It looks like you entered a numeric ID, which is not what I'm",
-                  s()%N%"looking for.")
+        swirl_out(s()%N%"يبدو انك ادخلت معرفا رقميا وليس ما انا عليه",
+                  s()%N%"تبحث عن")
       }
       # Check if the user stole the example courseid
       if(is_example) {
-        swirl_out(s()%N%"It looks like you entered the Course ID that I used as an",
-                  s()%N%"example, which is not what I'm looking for.")
+        swirl_out(s()%N%"يبدو انك ادخلت رقم الدوره التعليميه التي انا استخدمها كمثال ",
+                  s()%N%"اي انني لاابحث عن هذه الاجابه ")
       }
     }
-    swirl_out(s()%N%"Instead, I need your Course ID, which is the last",
-              s()%N%"part of the web address for your Coursera course.",
-              s()%N%"For example, if the homepage for your Coursera course was",
+    swirl_out(s()%N%" Coursera  انا اريد رقم الدوره التي كانت في اخر جزء من عنوان دوره",
+              s()%N%" الخاصه بك هي  Coursera  على سبيل المثال اذا كانت الصفحه الرئيسيه لدوره",
               s()%N%"'https://class.coursera.org/rprog-001',",
-              s()%N%"then your course ID would be 'rprog-001' (without the quotes).",
+              s()%N%"اذن سوف يكون رقم الدوره الخاصه بك هي'rprog-001' (without the quotes).",
               skip_after=TRUE)
   }
   courseid
 }
 
 getCreds <- function(e) {
-  cn <- make_pathname(attr(e$les, "course_name"))
+  cn <- make_pathname(attr(e$les, "اسم الدوره"))
   credfile <- file.path(e$udat, paste0(cn, ".txt"))
-  e$coursera <- digest(paste0("complete", paste0(
+  e$coursera <- digest(paste0("اكمل", paste0(
     rep("_", ifelse(is.null(e$skips), 0, e$skips)), collapse="")),
     algo="sha1", serialize = FALSE)
   
@@ -171,20 +170,20 @@ getCreds <- function(e) {
   while(!confirmed) {
     if(!file.exists(credfile) || need2fix) {
       courseid <- get_courseid()
-      email <- readline("Submission login (email): ")
-      passwd <- readline("Submission password: ")
+      email <- readline(":ادخل الايميل ")
+      passwd <- readline(":ادخل كلمه المرر ")
       writeLines(c(courseid, email, passwd), credfile)
       r <- c(courseid = courseid, email = email, passwd = passwd)
     } else {
       r <- readLines(credfile, warn=FALSE)
       names(r) <- c("courseid", "email", "passwd")
     }
-    swirl_out(s()%N%"Is the following information correct?", skip_after=TRUE)
-    message("Course ID: ", r['courseid'],
-            "\nSubmission login (email): ", r['email'], 
-            "\nSubmission password: ", r['passwd'])
-    yn <- c("Yes, go ahead!", 
-            "No, I need to change something.")
+    swirl_out(s()%N%"هل المعلومات التاليه صحيحه؟", skip_after=TRUE)
+    message(":رقم الكورس ", r['courseid'],
+            "\n:الايميل المدخل ", r['email'], 
+            "\n:كلمه المرور ", r['passwd'])
+    yn <- c("ممتاز، اكمل", 
+            "لا، اريد ان اغير شيئا ما")
     confirmed <- identical(select.list(yn, graphics=FALSE), yn[1])
     if(!confirmed) need2fix <- TRUE
   }
@@ -218,7 +217,7 @@ submitSolution <- function(email, submit.url, ch.resp, sid, output,
                  state = signature)
   params <- lapply(params, URLencode)
   result <- try(postForm(submit.url, .params = params), silent=TRUE)
-  if(is(result,"try-error")){
+  if(is(result,"حاول ، خطأ")){
     return(result)
   } else {
     s <- strsplit(result, "\\r\\n")[[1]]
